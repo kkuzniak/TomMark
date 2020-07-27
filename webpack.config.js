287ce6ac64330667fbec06ba048673c729429817
kkuzniak <applehisaki@gmail.com>
2020-07-27 23:10:05 +0200

    Add about page finished in like 90 percents

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HTMLWebpackPLugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;  
 
 module.exports = {
     entry: ['./src/js/app.js', './src/scss/app.scss'],
@@ -15,6 +17,31 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html',
             minify: false
+        }),
+        new HTMLWebpackPLugin({
+            filename: 'about.html',
+            template: './src/about.html',
+            minify: false,
+            inject: false
+        }),
+        new CopyPlugin({ 
+            patterns: [
+                {
+                    from: './src/fonts', to: './fonts'
+                },
+                {
+                    from: './src/images', to: './images'
+                },
+                {
+                    from: './src/icons', to: './icons'
+                },
+                {
+                    from: './src/videos', to: './videos'
+                }
+            ]
+        }),
+        new ImageminPlugin({
+            test: /\.(jpe?g|png|gif|svg)$/i
         })
     ],
     module: {
